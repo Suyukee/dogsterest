bbddde71f3dfd95dd8d5f38adfc7b0396304f36c
Suyukee <diefuck@ya.ru>
2024-07-07 20:03:00 +0300

    Added like button

--- src/components/blog-list.jsx
+++ src/components/blog-list.jsx
@@ -1,22 +1,24 @@
 import { useLikeDogMutation } from '@/store/dogs-api';
 
 function BlogList({ blogs }) {
-	function handleClick(blog) {
+	const [likeDog, { isError }] = useLikeDogMutation();
+
+	const handleLikeDog = async (blog) => {
 		const blogCopy = { ...blog };
-		blogCopy.likes = blog.likes + 1;
-		blogCopy.isLike = true;
+		blogCopy.isLike = !blogCopy.isLike;
+		blogCopy.isLike ? blogCopy.likes++ : blogCopy.likes--;
 		console.log(blogCopy);
-		useLikeDogMutation(blogCopy);
-		// window.localStorage.setItem(blog.id, JSON.stringify(blog.isLike));
-	}
+		window.localStorage.setItem(blogCopy.id, blogCopy.imageUrl);
+		await likeDog(blogCopy).unwrap();
+	};
 
 	return (
 		<article className="article">
 			{blogs.map((blog) => (
-				<button className="blog-preview" key={blog.id} onClick={() => handleClick(blog)}>
-					<p className="likes">
+				<button className="article__blog-preview" key={blog.id} onClick={() => handleLikeDog(blog)}>
+					<p className="blog-preview__likes">
 						{blog.likes}
-						<svg viewBox="0 0 24 24">
+						<svg className="likes__svg" viewBox="0 0 24 24">
 							<path
 								d="M2 9.1371C2 14 6.01943 16.5914 8.96173 18.9109C10 19.7294 11 20.5 12 20.5C13 20.5 14 19.7294 15.0383 18.9109C17.9806 16.5914 22 14 22 9.1371C22 4.27416 16.4998 0.825464 12 5.50063C7.50016 0.825464 2 4.27416 2 9.1371Z"
 								fill={blog.isLike ? '#f1356d' : '#ddd'}
@@ -25,11 +27,11 @@ function BlogList({ blogs }) {
 					</p>
 
 					{blog.imageUrl.substr(-3) === 'mp4' ? (
-						<video className="img" width={250} height={250} muted autoPlay loop>
+						<video className="blog-preview__img" width={250} height={250} muted autoPlay loop>
 							<source src={blog.imageUrl} type="video/mp4" />
 						</video>
 					) : (
-						<img className="img" src={blog.imageUrl} width={250} height={250} />
+						<img className="blog-preview__img" src={blog.imageUrl} width={250} height={250} />
 					)}
 				</button>
 			))}
--- src/store/dogs-api.ts
+++ src/store/dogs-api.ts
@@ -1,11 +1,29 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
+interface Post {
+	id: number;
+	name: string;
+}
+type PostsResponse = Post[];
+
 export const dogsApi = createApi({
 	reducerPath: 'dogsApi',
 	baseQuery: fetchBaseQuery({ baseUrl: 'https://dogsterest-data.onrender.com/' }),
+	tagTypes: ['Dogs'],
 	endpoints: (build) => ({
-		getDogs: build.query({
-			query: (limit = '') => `dogs?${limit && `_limit=${limit}`}`,
+		getDogs: build.query<PostsResponse, void>({
+			query: () => 'dogs',
+			providesTags: (result) =>
+				result
+					? [
+							...result.map(({ id }) => ({ type: 'Dogs' as const, id })),
+							{ type: 'Dogs', id: 'LIST' },
+					  ]
+					: [{ type: 'Dogs', id: 'LIST' }],
+		}),
+		getPost: build.query<Post, number>({
+			query: (id) => `post/${id}`,
+			providesTags: (result, error, id) => [{ type: 'Dogs', id }],
 		}),
 		likeDog: build.mutation({
 			query(data) {
@@ -16,6 +34,7 @@ export const dogsApi = createApi({
 					body,
 				};
 			},
+			invalidatesTags: [{ type: 'Dogs', id: 'LIST' }],
 		}),
 	}),
 });
